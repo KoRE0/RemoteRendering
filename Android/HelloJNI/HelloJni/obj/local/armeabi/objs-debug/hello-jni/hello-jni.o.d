8cdb466a56fecb6e0d602394715f4bd6b0c77557
Nils Lichtenberg <nlichtenberg@uni-koblenz.de>
2013-01-17 16:18:21 +0100

    Initial Android NDK Project
    first attempt in native imageprocessing (creating a plain color bitmap)

---
+++ Android/HelloJNI/HelloJni/obj/local/armeabi/objs-debug/hello-jni/hello-jni.o.d
@@ -0,0 +1,115 @@
+obj/local/armeabi/objs-debug/hello-jni/hello-jni.o: jni/hello-jni.cpp \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/string.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/malloc.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/jni.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/time.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/sys/time.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/stdint.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/linux/time.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/asm/siginfo.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/asm-generic/siginfo.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/android/log.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/android/bitmap.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/stdio.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/alloca.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/strings.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/memory.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/math.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/limits.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/asm/page.h
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/string.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/jni.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/time.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/sys/time.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/linux/time.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/asm/siginfo.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/asm-generic/siginfo.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/android/log.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/android/bitmap.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/stdio.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/strings.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/memory.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/math.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/limits.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+E:/Android/android-ndk-r8d-windows/android-ndk-r8d/platforms/android-8/arch-arm/usr/include/asm/page.h:
